43454ff4db9b7d51e7859e141b6b8cb0c1195f56
zRouth <zrouthier@gmail.com>
2015-12-03 14:27:30 -0700

    add enum and validations

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
 class Idea < ActiveRecord::Base
+  validates :title, :body, presence: true
+
+  enum quality: [:swill, :plausible, :genius]
 end
--- test/models/idea_test.rb
+++ test/models/idea_test.rb
@@ -4,7 +4,30 @@ class IdeaTest < ActiveSupport::TestCase
 
   test "should have a quality that defaults to 0" do
     idea = Idea.new
-    assert_equal(0, idea.quality)
+    assert_equal("swill", idea.quality)
   end
 
+  test "it should be invalid without a title or body" do
+    idea_without_title_or_body = Idea.new
+
+    refute(idea_without_title_or_body.valid?)
+  end
+
+  test "it should be invalid without a title" do
+    idea = Idea.new(body: "body")
+
+    refute(idea.valid?)
+  end
+
+  test "it should be invalid without a body" do
+    idea = Idea.new(title: "title")
+
+    refute(idea.valid?)
+  end
+
+  test "it is valid with a title and body" do
+    idea_with_title_and_body = Idea.new(title: "title", body: "body")
+
+    assert(idea_with_title_and_body.valid?)
+  end
 end
