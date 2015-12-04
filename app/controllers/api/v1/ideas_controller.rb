245c31fb14944fe476ddd06ca6ee559bcf792b4e
zRouth <zrouthier@gmail.com>
2015-12-04 10:44:30 -0700

    add controller test for update with invalid qualities

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -31,7 +31,7 @@ class Api::V1::IdeasController < ApplicationController
 private
 
   def idea_params
-    params.require(:idea).permit(:title, :body)
+    params.require(:idea).permit(:title, :body, :quality)
   end
 
 end
--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -81,4 +81,22 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
     assert_equal 'Updated Idea', ideas(:one).title
   end
 
+  test 'updates the quality of an idea' do
+    updated_content = { quality: 'plausible' }
+
+    put :update, id: ideas(:one), idea: updated_content, format: :json
+    ideas(:one).reload
+
+    assert_equal 'plausible', ideas(:one).quality
+  end
+
+  test 'update rejects invalid quality values' do
+    updated_content = { quality: 'invalid' }
+
+    put :update, id: ideas(:one), idea: updated_content, format: :json
+    ideas(:one).reload
+
+    assert_response 422
+  end
+
 end
